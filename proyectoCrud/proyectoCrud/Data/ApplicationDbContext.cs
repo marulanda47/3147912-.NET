3615c8c2cab2a2493a1b5726db82e48ecc6201cb
mateo <mateorp601@gmail.com>
2025-11-13 16:56:50 -0500

    agrego nuevo

---
+++ proyectoCrud/proyectoCrud/Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using proyectoCrud.Models;
+
+namespace proyectoCrud.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Producto>()
+                .Property(p => p.Precio)
+                .HasColumnType("decimal(18,2)");
+
+            // Relaciones opcionales (EF Core las detecta automáticamente, pero puedes ser explícito)
+            modelBuilder.Entity<Venta>()
+                .HasOne(v => v.Cliente)
+                .WithMany(c => c.Ventas)
+                .HasForeignKey(v => v.ClienteId);
+
+            modelBuilder.Entity<Venta>()
+                .HasOne(v => v.Producto)
+                .WithMany(p => p.Ventas)
+                .HasForeignKey(v => v.ProductoId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ proyectoCrud/proyectoCrud/Models/Venta.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace proyectoCrud.Models
+{
+    public class Venta
+    {
+        public int VentaId { get; set; }
+        public int ClienteId { get; set; }
+        public int ProductoId { get; set; }
+        public int Cantidad { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        // Relaciones
+        public Cliente? Cliente { get; set; }
+        public Producto? Producto { get; set; }
+    }
+}
